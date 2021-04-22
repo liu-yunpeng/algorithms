33358fe20d84df514906bfd73c4dfab177bc3f25
liu-yunpeng <liux3826@umn.edu>
2021-04-22 16:35:46 -0500

    Test Cases

--- Number of Inversions Merge Sort/number_of_inversion.py
+++ Number of Inversions Merge Sort/number_of_inversion.py
@@ -5,3 +5,7 @@ with open(input_file, 'r') as file:
 
 numbers = list(map(int, numbers))
 print(numbers)
+
+
+#numbers = [1,2,3,5,4,6]
+#numbers = [ 99888, 99889, 9989, 99890, 99891, 99892, 99893, 99894, 99895, 99896, 99897, 99898, 99899, 999, 9990, 99900]
