02ff77a15c4789cc9acbc234a07c163e99f23220
liu-yunpeng <liux3826@umn.edu>
2021-04-22 12:07:57 -0500

    Add test case

--- Karatsuba's algorithm/karatsuba_multiply.py
+++ Karatsuba's algorithm/karatsuba_multiply.py
@@ -15,3 +15,14 @@ def karatsuba(x,y):
 		ac = karatsuba(a,c)
 		bd = karatsuba(b,d)
 		ad_plus_bc = karatsuba(a+b,c+d) - ac - bd
+
+
+
+
+
+
+
+
+a = 3141592653589793238462643383279502884197169399375105820974944592
+b = 2718281828459045235360287471352662497757247093699959574966967627
+print(karatsuba(a,b))
