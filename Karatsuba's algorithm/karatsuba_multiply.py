1a175bcf0f194fa6c6971f9e968a2ac543cbf04a
liu-yunpeng <liux3826@umn.edu>
2021-04-22 12:06:31 -0500

    Update karatsuba_multiply.py

--- Karatsuba's algorithm/karatsuba_multiply.py
+++ Karatsuba's algorithm/karatsuba_multiply.py
@@ -2,3 +2,6 @@ def karatsuba(x,y):
 	"""Function to multiply 2 numbers in a more efficient manner than the grade school algorithm"""
 	if len(str(x)) == 1 or len(str(y)) == 1:
 		return x*y
+	else:
+		n = max(len(str(x)),len(str(y)))
+		nby2 = n / 2
