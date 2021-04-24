585a8b2453ca9ea720ed5fa2ea25e2f3465d1a5f
liu-yunpeng <liux3826@umn.edu>
2021-04-23 21:30:35 -0500

    Update quick_sort.cpp

--- Quick Sort/quick_sort.cpp
+++ Quick Sort/quick_sort.cpp
@@ -1,3 +1,22 @@
+// use the first element as pivot
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+
+    for (int j = low + 1; j <= high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i], &arr[low]);
+    return (i);
+}
+
+
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
