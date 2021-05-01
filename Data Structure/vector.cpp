bfb0bc2d91c81f3246fa63eb2a3c18aa2d198d4f
liu-yunpeng <liux3826@umn.edu>
2021-04-30 20:52:39 -0500

    Create vector.cpp

---
+++ Data Structure/vector.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> g1;
+
+    for (int i = 1; i <= 5; i++)
+        g1.push_back(i);
+
+    cout << "vector: ";
+    for (auto i = g1.begin(); i != g1.end(); ++i)
+        cout << *i << " ";
+
+
+
+
+	// the iterator constructor can also be used to construct from arrays:
+	int myints[] = {1, 2, 3, 4, 5};
+	vector<int> myvector (myints, myints + sizeof(myints) / sizeof(int) );
+
+	// iterate a vector
+	cout << "\nvector in reverse order:";
+	for (vector<int>::reverse_iterator it = myvector.rbegin(); it != myvector.rend(); ++it){
+		cout << *it << ' ';
+	}
+
+	// without using explicit pointers
+	cout << "\nvector easy loop:";
+	for (int n : myvector) {
+		cout << n << ' ';
+	}
+
+	return 0;
+}
