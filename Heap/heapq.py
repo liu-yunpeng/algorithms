bae255b26e04763294ff0f38b5971a8d6e6fb00a
liu-yunpeng <liux3826@umn.edu>
2021-06-25 15:05:19 -0500

    Update heapq.py

--- Heap/heapq.py
+++ Heap/heapq.py
@@ -1,18 +1,18 @@
 import heapq
 
 lst = [6, 7, 9, 4, 3, 5, 8, 10, 1]
-  
+
 # using heapify() to convert list into heap
 heapq.heapify(lst)
-  
+
 # get the largest k elements
 print(heapq.nlargest(3, lst))
-  
+
 # get the smallest k elements
 print(heapq.nsmallest(3, lst))
 
-heapq.heappush(lst,2)
-heapq.heappush(lst,12)
+heapq.heappush(lst, 2)
+heapq.heappush(lst, 12)
 
 print(lst)
 
