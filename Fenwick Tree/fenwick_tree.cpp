5e63f57f5e3810f805293c6329edcf829d2e7407
liu-yunpeng <liux3826@umn.edu>
2022-07-23 14:54:09 -0500

    fix update bug

--- Fenwick Tree/fenwick_tree.cpp
+++ Fenwick Tree/fenwick_tree.cpp
@@ -5,43 +5,43 @@ public:
     int n;
     NumArray(vector<int>& numss) {
         n = numss.size();
-        tree =  vector<int>(n+1);
+        tree =  vector<int>(n + 1);
         nums =  numss;
-        
-        for(int i = 0; i <n; i ++)
+
+        for (int i = 0; i < n; i ++)
         {
-            for(int j = i+1; j <=n ; j += j & (-j))
+            for (int j = i + 1; j <= n ; j += j & (-j))
             {
                 tree[j] += nums[i];
             }
         }
     }
-    
+
     void update(int index, int val) {
         int temp = val - nums[index];
-        
-            for(int j = index+1 ; j <= n; j += j & (-j))
-            {
-                tree[j] += temp;
-            }
-        
-        tree[index] = val; 
-        
+
+        for (int j = index + 1 ; j <= n; j += j & (-j))
+        {
+            tree[j] += temp;
+        }
+
+        nums[index] = val;  // NOT tree, but nums!! you are updating the origin arr
+
     }
-    
+
     int sumRange(int left, int right) {
         return getSum(right + 1) - getSum(left);
     }
-    
+
     int getSum(int idx)
     {
         int s = 0;
-        
-        for(int j = idx ; j > 0  ; j -= j & (-j))
+
+        for (int j = idx ; j > 0  ; j -= j & (-j))
         {
             s += tree[j];
         }
-        
+
         return s;
     }
 };
