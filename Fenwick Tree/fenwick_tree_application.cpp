37f3c3ed86cce5a3bcae535f04f13277f01f1b9a
liu-yunpeng <liux3826@umn.edu>
2022-07-26 15:51:30 -0500

    Update fenwick_tree_application.cpp
    make tree struct standalone.
    
    separate offset to a function

--- Fenwick Tree/fenwick_tree_application.cpp
+++ Fenwick Tree/fenwick_tree_application.cpp
@@ -1,65 +1,44 @@
-class Solution {
-public:
-
-    vector<int> tree;
-    vector<int> nums;
-
-    vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-
-        this->nums = nums;
-
-        int offset = (int) 1e4 + 1;
-
-        vector<int> res(n);
-        res[n - 1] = 0;
-
-        int tree_n = offset * 2;
-
-        tree = vector<int>(tree_n);
-
-        // update the last val in nums
-        for (int j = offset; j <= tree_n; j += j & (-j))
-        {
-            tree[j] += nums[n - 1];
+struct BIT {
+    vector<int> bit;
+    int n;
+    BIT() {
+        n = 1e4 * 2 + 1;
+        bit.assign(n, 0);
+    }
+    void update(int idx, int val) {
+        while (idx < n) {
+            bit[idx] += val;
+            idx = idx | (idx + 1);
         }
-
-        // cout<<"ok so far"<<endl;
-
-        for (int i = n - 2 ; i >= 0; i--)
-        {
-            if (nums[i] > nums[i + 1])
-            {
-                res[i] = res[i + 1] + 1;
-
-                // only update if current one is larger
-                for (int j = nums[i = 1] + offset; j <= n + offset; j += j & (-j))
-                {
-                    tree[j] += 1;
-                    tree[nums[i + 1] + offset]++;
-                    // cout<<'h'<<endl;
-                }
-            }
-            else
-            {
-                int temp = 0;
-
-                for (int j = nums[i] + offset; j > 0; j -= j & (-j))
-                {
-                    temp +=  tree[j];
-                }
-
-                res[i] = temp;
-            }
-
-            // cout<<"ok so fafarr"<<endl;
+    }
 
 
+    int query(int idx) {
+        int s = 0;
+        while (idx >= 0) {
+            s += bit[idx];
+            idx = (idx & (idx + 1)) - 1;
         }
-
-
-        return res;
-
-
+        return s;
     }
 };
+
+int getidx(int idx) {
+    return idx + 1e4;
+}
+
+vector<int> countSmaller(vector<int>& nums) {
+    vector<int> ans;
+    BIT bit;
+    for (int i : nums) {
+        bit.update(getidx(i), 1);
+    }
+
+    for (int i : nums) {
+        ans.push_back(bit.query(getidx(i - 1)));
+        bit.update(getidx(i), -1);
+    }
+
+    return ans;
+
+}
